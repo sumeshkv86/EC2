cad4e6a8cd53f55334a2f634ef364c97a295ba4f
shubham781999 <138647879+shubham781999@users.noreply.github.com>
2024-05-19 15:21:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-    region = "us-east-1"  
+   region     = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-05fa00d4c63e32376"
+   subnet_id     = "subnet-0a70a567c36632424"
+   key_name      = "keypair"
+   instance_type =  "t2.micro"
+   count = 1
+   associate_public_ip_address = var.enable_public_ip
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+
 }
+
+variable "enable_public_ip" {
+  description = "Enable public IP address"
+  type        = bool
+  default     = true
+} 
